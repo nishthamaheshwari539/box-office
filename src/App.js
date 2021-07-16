59e515e12b64fc2041d597bafdcdf50bfd646330
Nishhh <nishthamaheshwari123@gmail.com>
2021-07-16 18:36:20 +0530

    added navbar and page comp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom'
 
+import Navs from './components/Navs';
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+
 function App() {
-  return <Switch>
-
-    <Route exact path="/">
-      This is Home Page.
-    </Route>
-    <Route exact path="/starred">
-      This is Starred.
-    </Route>
-    <Route >
-      This is 404 page.
-    </Route>
-
-
-  </Switch>
+  return (
+    <div>
+       <Navs/>
+      <Switch>
+        <Route exact path="/">
+           <Home />
+        </Route>
+        <Route exact path="/starred">
+            <Starred />
+        </Route>
+        <Route >
+           <div>Not Found</div>
+        </Route>
+      </Switch>
+  
+  </div>
+  );
 }
 
 export default App;
