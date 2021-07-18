fa966d7490cf4e4d49ef7cb65d59796be007cac5
Nishhh <nishthamaheshwari123@gmail.com>
2021-07-18 21:33:50 +0530

    Updates

--- precache-manifest.543b02dad65e1e86456e3c6916c74291.js
+++ precache-manifest.543b02dad65e1e86456e3c6916c74291.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f05e8470a1b708708f28bcaa6be03cb0",
+    "revision": "648ad2e2e7a084fadcc466bb8a48de36",
     "url": "/box-office/index.html"
   },
   {
-    "revision": "81b955fe08544d64b239",
+    "revision": "5011e2e1f8eb8bc4be2a",
     "url": "/box-office/static/css/main.93052401.chunk.css"
   },
   {
-    "revision": "9a2c874b6a175a0122b7",
-    "url": "/box-office/static/js/2.e4522165.chunk.js"
+    "revision": "610bdc9597ae3714515e",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/box-office/static/js/2.e4522165.chunk.js.LICENSE.txt"
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81b955fe08544d64b239",
-    "url": "/box-office/static/js/main.67fc5d00.chunk.js"
+    "revision": "5011e2e1f8eb8bc4be2a",
+    "url": "/box-office/static/js/main.ef392ed1.chunk.js"
   },
   {
     "revision": "c850b6c555a3d841251e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/box-office/precache-manifest.9f6b7d220c4b26ba2dc35e8ce1b69900.js"
+  "/box-office/precache-manifest.543b02dad65e1e86456e3c6916c74291.js"
 );
 
 self.addEventListener('message', (event) => {
